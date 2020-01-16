91c693a12e3cacfd859c6812156ae5b9d6556476
VMerk <mikulenko.vladimir@yandex.ru>
2020-01-16 22:00:33 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+{
+    const colorPicker = document.querySelector('#main-color');
+    const mainBlock = document.querySelector('#main');
+
+    setColor(colorPicker.value);
+
+    colorPicker.addEventListener("change", watchColorPicker);
+    mainBlock.addEventListener("mouseover", inverseBackgroundColor);
+    mainBlock.addEventListener("mouseout", inverseBackgroundColor);
+      
+    function watchColorPicker(event) {    
+        setColor(event.target.value);
+        inverseBackgroundColor();
+    }
+      
+    function setColor(color) {
+        mainBlock.style.backgroundColor = color;
+    }
+      
+    function inverseBackgroundColor() {
+        let rgb = parseRGBFromString(getComputedStyle(mainBlock).backgroundColor);
+        rgb = rgb.map(color => 255 - +color);
+        mainBlock.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    }
+      
+    /**
+    * return string array
+    **/
+    function parseRGBFromString(rgbString) {
+        return rgbString.match(/\d+/g);
+    }
+}
